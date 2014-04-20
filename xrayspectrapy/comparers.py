11d3b410eb84bffca581e1583842f35a95428ff7
chad <cavoegele@gmail.com>
2014-04-20 18:25:48 -0500

    add least squares comparison. forgot to add test data before.

---
+++ xrayspectrapy/comparers.py
@@ -0,0 +1,15 @@
+import math
+
+def least_squares(image1, image2):
+    # image frequencies should be the same length as the distances if they were
+    # not modified after construction
+    if len(image1.distances) != len(image2.distances):
+        raise ValueError("images must be the same length.")
+
+    for i in range(0, len(image1.distances)):
+        if image1.distances[i] != image2.distances[i]:
+            raise ValueError("images' distances must match")
+
+    return math.sqrt(sum(((x-y)*(x-y) for (x,y)
+                in zip(image1.frequencies, image2.frequencies))))
+
---
+++ xrayspectrapy/test/test_comparers.py
@@ -0,0 +1,25 @@
+import unittest
+import xrayspectrapy as xsp
+
+class ImageComprarerTests(unittest.TestCase):
+    def data_for_test1(self):
+        return xsp.Image([2, 2.5, 3, 3.5, 4, 4.5, 5],
+                [0.5, 0.4, 0.3, 0.8, 0.9, 0.1, 0.1])
+
+    def data_for_test2(self):
+        return xsp.Image([2, 2.5, 3, 3.5, 4, 4.5, 5],
+                [0.1, 0.3, 0.2, 0.9, 0.0, 0.2, 0.3])
+
+    def test_least_squares(self):
+        im1 = self.data_for_test1()
+        im2 = self.data_for_test2()
+        dist = xsp.comparers.least_squares(im1, im2)
+        self.assertAlmostEqual(1.0247, dist, 4)
+
+    def test_str(self):
+        a = xsp.Atom(5, 8, 9)
+        self.assertEqual(str(a), '(5, 8, 9)')
+
+if __name__ == '__main__':
+    unittest.main()
+
