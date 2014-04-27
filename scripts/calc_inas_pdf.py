4cb4e11620827a011c1413764f7770c2987c8d3a
chad <cavoegele@gmail.com>
2014-04-27 12:44:50 -0500

    updated rdf calc on doc, added experimental vs calc section

---
+++ scripts/calc_inas_pdf.py
@@ -0,0 +1,30 @@
+import sys, os
+parentdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parentdir)
+
+import xrayspectrapy as xsp
+from scipy import interpolate
+
+atom_data = [(0.00,0.00,0.00,"In1"),
+             (0.00,0.50,0.50,"In1"),
+             (0.50,0.00,0.50,"In3"),
+             (0.50,0.50,0.00,"In4"),
+             (0.25,0.25,0.25,"As1"),
+             (0.75,0.75,0.25,"As2"),
+             (0.75,0.25,0.75,"As3"),
+             (0.25,0.75,0.75,"As4")]
+c = 6.0583
+
+s = xsp.Structure([xsp.Atom(c*x, c*y, c*z, l) for (x,y,z,l) in atom_data], c)
+
+bins = xsp.pdf.calc_bins(1, 10, 200)
+im = xsp.pdf.calc_pdf(s, 10, bins)
+im = xsp.pdf.smooth_image(im, 0.005)
+# print(im)
+
+expt_filename = os.path.expanduser('~/work/inas_fig4.csv')
+im = xsp.datadefs.image.fromFile(expt_filename)
+f = interpolate.interp1d(im.distances, im.frequencies, kind = 'cubic')
+freqnew = [f(y) for y in bins[2:(len(bins)-1)]]
+imnew = xsp.Image(bins[2:(len(bins)-1)], freqnew)
+print(imnew)
