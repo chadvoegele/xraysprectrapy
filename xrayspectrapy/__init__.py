a1fa952a0c34a5ae87461f05757525c1da784276
chad <cavoegele@gmail.com>
2014-05-11 21:16:01 -0500

    add peakdet to library

--- xrayspectrapy/__init__.py
+++ xrayspectrapy/__init__.py
@@ -3,3 +3,4 @@ from xrayspectrapy.datadefs.structure import Structure
 from xrayspectrapy.datadefs.image import Image
 from xrayspectrapy import pdf
 from xrayspectrapy import comparers
+from xrayspectrapy import peakdet
---
+++ xrayspectrapy/test/test_peakdet.py
@@ -0,0 +1,39 @@
+import unittest
+import xrayspectrapy as xsp
+
+class PeakDetTests(unittest.TestCase):
+    def data_for_test1(self):
+        dists = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 
+                 8.5, 9, 9.5, 10]
+        freqs = [0, 0.5, 1, 0.5, 0, 0.1, 0, 10, 9, 8, 7, 0, 0.5, 0, 0.4, 0, 0.8,
+                 0.9, 1]
+        return xsp.Image(dists, freqs)
+
+    def test_peaks1(self):
+        im1 = self.data_for_test1()
+        (dists, peaks) = xsp.peakdet.image_peakdet(im1, 0.0001)
+        expected_dists = [ 2, 3.5, 4.5, 7, 8 ]
+        expected_peaks = [ 1, 0.1, 10, 0.5, 0.4 ]
+
+        for (actual, expected) in zip(dists, expected_dists):
+            self.assertAlmostEqual(actual, expected, 4)
+
+        for (actual, expected) in zip(peaks, expected_peaks):
+            self.assertAlmostEqual(actual, expected, 4)
+
+    def test_peaks2(self):
+        im1 = self.data_for_test1()
+        (dists, peaks) = xsp.peakdet.image_peakdet(im1, 0.9)
+        expected_dists = [ 2, 4.5 ]
+        expected_peaks = [ 1, 10 ]
+
+        for (actual, expected) in zip(dists, expected_dists):
+            self.assertAlmostEqual(actual, expected, 4)
+
+        for (actual, expected) in zip(peaks, expected_peaks):
+            self.assertAlmostEqual(actual, expected, 4)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
