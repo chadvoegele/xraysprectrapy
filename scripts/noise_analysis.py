80205405c8d59e5c667068d4eeb1c228ed54546f
chad <cavoegele@gmail.com>
2014-05-17 16:44:48 -0500

    clean up analysis scripts

--- scripts/noise_analysis.py
+++ scripts/noise_analysis.py
@@ -1,4 +1,5 @@
 import xrayspectrapy as xsp
+import os
 import math
 import numpy as np
 
@@ -11,6 +12,7 @@ def main():
     exptFiles = [os.path.join(filedir, f) for f in os.listdir(filedir)
                    if os.path.isfile(os.path.join(filedir, f))
                    if ('Expt' in f or 'expt' in f)]
+    exptFiles = [os.path.expanduser('~/work/inas_fig4n.csv')]
 
     #for afile in exptFiles:
     #    filepath = os.path.expanduser(os.path.join(directory, afile))
@@ -22,10 +24,11 @@ def main():
 
     exptImages = [xsp.datadefs.image.fromFile(f) for f in exptFiles]
 
-    img = exptImages[3]
-    # print(str(img))
-    # (maxpeak, minpeak) = peakdet(img.frequencies, 0.0001, img.distances)
-    # print('\n'.join([str(x) + '\t' + str(y) for (x,y) in maxpeak]))
+    img = exptImages[0]
+    img = xsp.pdf.normalize_image(img)
+    print(str(img))
+    (maxpeak, minpeak) = peakdet(img.frequencies, 0.0001, img.distances)
+    print('\n'.join([str(x[0]) + '\t' + str(x[1]) for x in maxpeak]))
 
     #distances = [d[0] for img in exptImages 
     #    for maxpeak in peakdet(img.frequencies, 0.0001, img.distances)
@@ -42,23 +45,23 @@ def main():
     #print('\n'.join([str(b[1]) + '\t' + str(b[0]) 
     #    for b in zip(hist[0], hist[1])]))
 
-    matches = [('ExptGaAs.dat', 'CalcGaAs.dat'),
-               ('ExptInAs.dat', 'CalcInAs.dat'),
-               ('SiLiExpt1.dat', 'SiLiCalc10001.dat')]
+    #matches = [('ExptGaAs.dat', 'CalcGaAs.dat'),
+    #           ('ExptInAs.dat', 'CalcInAs.dat'),
+    #           ('SiLiExpt1.dat', 'SiLiCalc10001.dat')]
 
-    allPeakDiffs = []
-    for match in matches:
-        exptimg = xsp.datadefs.image.fromFile(os.path.join(filedir, match[0]))
-        calcimg = xsp.datadefs.image.fromFile(os.path.join(filedir, match[1]))
-        diffFreq = [e - c 
-                for (e, c) in zip(exptimg.frequencies, calcimg.frequencies)]
-        diffImg = xsp.Image(exptimg.distances, diffFreq)
-        (maxpeak, minpeak) = peakdet(diffImg.frequencies, 0.0001, 
-                diffImg.distances)
-        peakDiffs = [p[1] for p in maxpeak]
-        allPeakDiffs.extend(peakDiffs)
+    #allPeakDiffs = []
+    #for match in matches:
+    #    exptimg = xsp.datadefs.image.fromFile(os.path.join(filedir, match[0]))
+    #    calcimg = xsp.datadefs.image.fromFile(os.path.join(filedir, match[1]))
+    #    diffFreq = [e - c 
+    #            for (e, c) in zip(exptimg.frequencies, calcimg.frequencies)]
+    #    diffImg = xsp.Image(exptimg.distances, diffFreq)
+    #    (maxpeak, minpeak) = peakdet(diffImg.frequencies, 0.0001, 
+    #            diffImg.distances)
+    #    peakDiffs = [p[1] for p in maxpeak]
+    #    allPeakDiffs.extend(peakDiffs)
 
-    print('\n'.join((str(p) for p in allPeakDiffs)))
+    #print('\n'.join((str(p) for p in allPeakDiffs)))
     # hist = np.histogram(allPeakDiffs, [-0.01 + 0.001*d for d in range(0,22)])
     # print('\n'.join([str(b[1]) + '\t' + str(b[0]) 
         # for b in zip(hist[0], hist[1])]))
--- scripts/synthetic_expr_analysis.py
+++ scripts/synthetic_expr_analysis.py
@@ -1,4 +1,5 @@
 import xrayspectrapy as xsp
+import os
 import math
 import numpy as np
 import random
@@ -7,137 +8,60 @@ import sys
 from numpy import NaN, Inf, arange, isscalar, asarray, array
 
 def main():
-    directory = '~/work/all_rfdata_smoothed_unique/'
+    directory = '~/work/all_rfdata_unique/'
     filedir = os.path.expanduser(directory)
     calcFiles = [os.path.join(filedir, f) for f in os.listdir(filedir)
-                   if os.path.isfile(os.path.join(filedir, f))
-                   if ('Calc' in f or 'calc' in f)]
-
+                   if os.path.isfile(os.path.join(filedir, f))]
+                   # if ('CalcInAs' in f or 'calc' in f)]
+    
+    # output 100 random images
     #img = xsp.datadefs.image.fromFile(calcFiles[0])
-    #imgFilePath = os.path.join(os.path.expanduser('~/work'), 'rand1.jpg')
-    #xsp.datadefs.image.saveAsSpectrumImage(imgFilePath, noisifyImage(img), 50, 20)
+    #img = xsp.pdf.normalize_image(img)
+    #stdev = 0.06
+    #mean = 0.004
+    #n = 100
+    #output = [img.distances]
+    #for i in range(0, n):
+    #    randFn = lambda x: xsp.pdf.rand_normal_peaks(x, mean, stdev, 7, 14)
+    #    noiseIm = xsp.pdf.noisify_image(img, randFn, 0.004)
+    #    output.append(noiseIm.frequencies)
+    #print('\n'.join(['\t'.join([str(x) for x in out]) for out in output]))
 
     calcImages = [xsp.datadefs.image.fromFile(f) for f in calcFiles]
+    calcSmoothedImages = [xsp.pdf.smooth_image(im, 0.004) for im in calcImages]
+    calcSmoothedImages = [xsp.pdf.normalize_image(im) for im in calcSmoothedImages]
 
-    labels = [im.label for im in calcImages]
-    n = 500
+    vals = [(0, 0),
+            (0.0015, 0),
+            (0.0036, 0),
+            (0.0036, 0.003),
+            (0.0036, 0.01),
+            (0.0036, 0.02),
+            (0.0036, 0.03),
+            (0.0036, 0.04),
+            (0.0036, 0.05),
+            (0.0036, 0.06)]
+
+    n = 50
+    for val in vals:
+        accuracy = getAccuracy(calcImages, calcSmoothedImages, n, val[0],
+                                    val[1])
+        print("Mean: " + str(val[0]) + ', StDev: ' + str(val[1]) + \
+                ', Accuracy: ' + str(accuracy))
+
+def getAccuracy(calcImages, calcSmoothedImages, n, mean, stdev):
     success = 0
     for i in range(0, n):
         randIndex = random.randrange(0, len(calcImages))
         randImg = calcImages[randIndex]
-        exprImg = noisifyImage(randImg)
+        randFn = lambda x: xsp.pdf.rand_normal_peaks(x, mean, stdev, 7, 14)
+        exprImg = xsp.pdf.noisify_image(randImg, randFn, 0.004)
 
-        dists = [xsp.comparers.l1_norm(exprImg, calcImg)
-                 for calcImg in calcImages]
+        dists = [xsp.comparers.l2_norm(exprImg, calcImg)
+                 for calcImg in calcSmoothedImages]
         if np.argmin(dists) == randIndex:
             success = success + 1
     
-    print(success / n)
-
-
-def randPeaks():
-    countPeaks = random.randint(7, 14)
-    peaks = []
-    for i in range(0, countPeaks):
-        peakHeight = random.normalvariate(0.0036, 0.0039)
-        peakLocation = 1.92 + 0.04*random.randint(0, 127)
-        peaks.append([peakLocation, peakHeight])
-
-    return peaks
-
-def normalizeImage(im):
-    minFreq = min(im.frequencies)
-    newFreq = [f - minFreq for f in im.frequencies]
-    sumFreq = sum(newFreq)
-    newFreq = [f / sumFreq for f in newFreq]
-    return xsp.Image(im.distances, newFreq, im.label)
-
-def noisifyImage(img):
-    (maxpeak, minpeak) = peakdet(img.frequencies, 0.0001, img.distances)
-    newImPeaks = randPeaks() 
-    newImPeaks.extend([[d[0], d[1]] for d in maxpeak])
-
-    newFreqs = []
-    for dist in img.distances:
-        newFreqs.append(sum([d[1] for d in newImPeaks 
-            if (d[0] > dist - 0.001 and d[0] < dist + 0.001)]))
-
-    newIm = xsp.Image(img.distances, newFreqs)
-    newIm = normalizeImage(newIm)
-    newIm = xsp.pdf.smooth_image(newIm, 0.004)
-
-    return newIm
-
-# https://gist.github.com/endolith/250860
-def peakdet(v, delta, x = None):
-    """
-    Converted from MATLAB script at http://billauer.co.il/peakdet.html
-    
-    Returns two arrays
-    
-    function [maxtab, mintab]=peakdet(v, delta, x)
-    %PEAKDET Detect peaks in a vector
-    %        [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
-    %        maxima and minima ("peaks") in the vector V.
-    %        MAXTAB and MINTAB consists of two columns. Column 1
-    %        contains indices in V, and column 2 the found values.
-    %      
-    %        With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
-    %        in MAXTAB and MINTAB are replaced with the corresponding
-    %        X-values.
-    %
-    %        A point is considered a maximum peak if it has the maximal
-    %        value, and was preceded (to the left) by a value lower by
-    %        DELTA.
-    
-    % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
-    % This function is released to the public domain; Any use is allowed.
-    
-    """
-    maxtab = []
-    mintab = []
-       
-    if x is None:
-        x = arange(len(v))
-    
-    v = asarray(v)
-    
-    if len(v) != len(x):
-        sys.exit('Input vectors v and x must have same length')
-    
-    if not isscalar(delta):
-        sys.exit('Input argument delta must be a scalar')
-    
-    if delta <= 0:
-        sys.exit('Input argument delta must be positive')
-    
-    mn, mx = Inf, -Inf
-    mnpos, mxpos = NaN, NaN
-    
-    lookformax = True
-    
-    for i in arange(len(v)):
-        this = v[i]
-        if this > mx:
-            mx = this
-            mxpos = x[i]
-        if this < mn:
-            mn = this
-            mnpos = x[i]
-        
-        if lookformax:
-            if this < mx-delta:
-                maxtab.append((mxpos, mx))
-                mn = this
-                mnpos = x[i]
-                lookformax = False
-        else:
-            if this > mn+delta:
-                mintab.append((mnpos, mn))
-                mx = this
-                mxpos = x[i]
-                lookformax = True
- 
-    return array(maxtab), array(mintab)
+    return success / n
 
 main()
