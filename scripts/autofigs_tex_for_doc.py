edbb1cd5c75ee5a328c22cfb8b77fb5d6bb287ff
chad <cavoegele@gmail.com>
2014-11-03 17:38:09 -0600

    collect figs, autotex needed for doc in one script

---
+++ scripts/autofigs_tex_for_doc.py
@@ -0,0 +1,10 @@
+import eigenfaces_analysis
+
+eigenfaces_analysis.plotBestMatches(3)
+eigenfaces_analysis.plotBestMatches(10)
+eigenfaces_analysis.plotBestMatches(128)
+
+import sparse_rep
+
+sparse_rep.plotBestMatches()
+sparse_rep.plotCompositeExptAnalysis()
